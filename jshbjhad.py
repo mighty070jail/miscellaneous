494f95dba8532279d2c22b7ec20a4c61cf066f3a
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-12-06 16:16:20 +0530

    Update jshbjhad.py

--- jshbjhad.py
+++ jshbjhad.py
@@ -6,23 +6,24 @@ A set contains 1, 2, or 3 sticks.
 The player who takes the last stick is the loser.
 The number of sticks in the set is to be input.
 '''
+
 player1=input("Enter Name of Player-1:")
 player2=input("Enter Name of Player-2:")
 count=0
 number_of_sticks=16
 while number_of_sticks!=0:
     if number_of_sticks!=0:
+        print(number_of_sticks)
         print(f"{player1}'s Turn")
         sticks_taken = int(input("Number of sticks to be taken?"))
-        count+=1
         number_of_sticks = number_of_sticks - sticks_taken
+        player=player1
     if number_of_sticks!=0:
+        print(number_of_sticks)
         print(f"{player2}'s Turn")
         sticks_taken = int(input("Number of sticks to be taken?"))
-        count+=1
+        player=player2
         number_of_sticks = number_of_sticks - sticks_taken
-    if number_of_sticks==0:
-        if count%2==0:
-            print(f"{player2} Wins!")
-        else:
-            print(f"{player1} Wins!")
+if number_of_sticks==0:
+    print(f"{player}, is the loser.")
+    
